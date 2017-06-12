7d3c0c6398bc60830762f82014e05258c507b4ce
Raphassit Leelachaikul <raphassitlee@gmail.com>
2017-06-12 12:47:34 +0700

    Remove default calendar prop

--- DatePickerIOS.js
+++ DatePickerIOS.js
@@ -26,8 +26,7 @@ const DatePickerIOS = React.createClass({
 
   getDefaultProps: function () {
     return {
-      mode: 'datetime',
-      calendar: 'gregorian'
+      mode: 'datetime'
     };
   },
 
